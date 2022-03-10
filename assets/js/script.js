b99125932acc985ce25f587d63a184c6ea8cc237
Samantha Wolff <jasonwolff@Samanthas-MacBook-Air.local>
2022-03-10 01:43:40 -0600

    add rest of color coding to time-blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,18 @@ $("#currentDay").html("Current Date: " + todaysDate);
 var currentHour = moment().hour();
 
 function colorCode() {
+    // 9AM color coding and test dummy
     if (currentHour === 9) {
         $("#schedule-text").addClass("present");
     }
     else if (currentHour > 9) {
         $("#schedule-text").addClass("past");
     }
-    else {
+    else if (currentHour < 9) {
         $("#schedule-text").addClass("future");
-    };
+    }; 
 
+    // 10AM color coding
     if (currentHour === 10) {
         $("#schedule-text-1").addClass("present");
     }
@@ -22,6 +24,83 @@ function colorCode() {
     }
     else {
         $("#schedule-text-1").addClass("future");
+    };
+
+    // 11AM color coding
+    if (currentHour === 11) {
+        $("#schedule-text-2").addClass("present");
+    }
+    else if (currentHour > 11) {
+        $("#schedule-text-2").addClass("past");
+    }
+    else {
+        $("#schedule-text-2").addClass("future");
+    };
+
+    // 12PM color coding
+    if (currentHour === 12) {
+        $("#schedule-text-3").addClass("present");
+    }
+    else if (currentHour > 12) {
+        $("#schedule-text-3").addClass("past");
+    }
+    else {
+        $("#schedule-text-3").addClass("future");
+    };
+
+    // 1PM color coding
+    if (currentHour === 1) {
+        $("#schedule-text-4").addClass("present");
+    }
+    else if (currentHour > 1) {
+        $("#schedule-text-4").addClass("past");
+    }
+    else {
+        $("#schedule-text-4").addClass("future");
+    };
+
+    // 2PM color coding
+    if (currentHour === 2) {
+        $("#schedule-text-5").addClass("present");
+    }
+    else if (currentHour > 2) {
+        $("#schedule-text-5").addClass("past");
+    }
+    else {
+        $("#schedule-text-5").addClass("future");
+    };
+
+    // 3PM color coding
+    if (currentHour === 3) {
+        $("#schedule-text-6").addClass("present");
+    }
+    else if (currentHour > 3) {
+        $("#schedule-text-6").addClass("past");
+    }
+    else {
+        $("#schedule-text-6").addClass("future");
+    };
+
+    // 4PM color coding
+    if (currentHour === 4) {
+        $("#schedule-text-7").addClass("present");
+    }
+    else if (currentHour > 4) {
+        $("#schedule-text-7").addClass("past");
+    }
+    else {
+        $("#schedule-text-7").addClass("future");
+    };
+
+    // 5PM color coding
+    if (currentHour === 5) {
+        $("#schedule-text-8").addClass("present");
+    }
+    else if (currentHour > 5) {
+        $("#schedule-text-8").addClass("past");
+    }
+    else {
+        $("#schedule-text-8").addClass("future");
     };
 
 
